1b5e59e8af210be61081f71e4d5c2fccdc75b81e
nanoguo <nanoguo@qq.com>
2022-12-25 19:11:21 +0800

    remove unused key

--- coc/coc_sdk.py
+++ coc/coc_sdk.py
@@ -1,11 +1,11 @@
 import requests
+import settings
 import urllib.parse
 
 clan_tag = urllib.parse.quote_plus('#2802GQRV8')
 api_url = 'https://api.clashofclans.com/v1'
-API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBkYjNiZWUwLWU0MjUtNDc3ZS05ZDQ1LWQwZmUyOWQ1YTUzOSIsImlhdCI6MTY2OTA0MDU1Mywic3ViIjoiZGV2ZWxvcGVyL2Q2OWE3MzFhLTk1ZTUtYTc1OS01MGMzLWUxMWE0MjQ4ZjAwNCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjExMy4xMTAuMTY1LjkxIl0sInR5cGUiOiJjbGllbnQifV19.mcfuthM2cO06s_1QCf6DXd9dDdQdF8zQlenyp3Wb3edhVrPRUP1HwRk0CaWNsxxhDXeoWuan8G3oWHZ9cgKMgw'
 headers = {
-    'Authorization': f'Bearer {API_KEY}'
+    'Authorization': f'Bearer {settings.COC_PRIVATE_KEY}'
 }
 
 
